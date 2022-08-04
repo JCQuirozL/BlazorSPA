89d81c02ff1432409874ad176ba5d6c7b2815241
carrillolj <carrillolj@HDFCARRILLOLJ>
2022-08-04 11:21:46 -0500

    Agregar archivos de proyecto.

---
+++ BlazorSPA/Data/Models/Device.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSPA.Models
+{
+    public abstract class Device
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Color { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public abstract decimal GetPrice();
+    }
+}
---
+++ BlazorSPA/Data/Models/Laptop.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace BlazorSPA.Models
+{
+    
+    using System.ComponentModel.DataAnnotations;
+    public class Laptop : Device
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Processor { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Ram { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string HDD { get; set; }
+
+        [Required]
+        public int Screen { get; set; }
+        public float Discount { get; set; }
+
+        public override decimal GetPrice()
+        {
+            decimal discount;
+            discount = (decimal)(Discount / 100) * Price;
+
+            return (Price - discount);
+        }
+    }
+}
