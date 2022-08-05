96610184958890d537995e1587072681523fbd01
JCQuirozL <luisjorgecarrilloq@gmail.com>
2022-08-05 10:19:22 -0500

    CRUD complete,  SweetAlert added

---
+++ BlazorSPA/Helpers/IJSExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorSPA.Helpers
+{
+    public static class IJSExtensions
+    {
+        public async static Task<object> MostrarMensaje(this IJSRuntime js, string Mensaje)
+        {
+            return await js.InvokeAsync<object>("Swal.fire", Mensaje);  
+        }
+
+        public async static Task<Boolean> Confirm(this IJSRuntime js, string titulo, string mensaje, TipoSweetAlert tipo)
+        { 
+            return await js.InvokeAsync<Boolean>("CustomConfirm", titulo, mensaje, tipo);
+        }
+
+        public enum TipoSweetAlert
+        {
+            question, warning, error, success, info
+        }
+    }
+}
---
+++ BlazorSPA/wwwroot/js/Utilerias.js
@@ -0,0 +1,20 @@
+﻿function CustomConfirm(titulo, mensaje, tipo) {
+    return new Promise((resolve) => {
+        Swal.fire({
+            title: titulo,
+            text: mensaje,
+            icon: tipo,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, borrar',
+            cancelButtonText:'Cancelar',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+    });
+}
