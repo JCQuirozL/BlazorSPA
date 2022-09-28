d9021351c5834c35728e8804cd100bf1414e9be3
Jorge Carrillo <luisjorgecarrilloq@gmail.com>
2022-09-27 19:20:03 -0500

    Edit policies completed hoooray

---
+++ PoliciesBlazorApp/PoliciesBlazorApp/Models/PatchPolicies.cs
@@ -0,0 +1,12 @@
+﻿namespace PoliciesBlazorApp.Models
+{
+    public class PatchPolicies
+    {
+        public Policy Policy { get; set; }
+
+        public PatchPolicies(Policy policy)
+        {
+            Policy = policy;
+        }
+    }
+}
---
+++ PoliciesBlazorApp/PoliciesBlazorApp/Models/Policy.cs
@@ -0,0 +1,25 @@
+﻿namespace PoliciesBlazorApp.Models
+{
+    public class Policy
+    {
+        public String PolicyNumber { get; set; }
+
+        public String PaymentFolio { get; set; }
+
+        public String ReferenceId { get; set; }
+
+        public String Bank { get; set; }
+
+        public String Invoice { get; set; }
+
+        public String AccountNumber { get; set; }
+
+        public DateTime DocumentDate { get; set; }
+
+        public Decimal DepositAmount { get; set; }
+
+        public Boolean Validated { get; set; }
+
+
+    }
+}
