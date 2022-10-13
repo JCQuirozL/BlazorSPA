eb516e9e02f9822e864d36811fbf14c92c48b89f
Jorge Carrillo <luisjorgecarrilloq@gmail.com>
2022-10-12 19:11:24 -0500

    w/ white logo and custom styles

--- PoliciesBlazorApp/PoliciesBlazorApp/Models/LeasingData.cs
+++ PoliciesBlazorApp/PoliciesBlazorApp/Models/LeasingData.cs
@@ -1,14 +1,27 @@
-﻿namespace PoliciesBlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PoliciesBlazorApp.Models
 {
     public class LeasingData
     {
         public DateTime? ValidationDate { get; set; }
+
+        [Required(ErrorMessage = "El número de folio no puede quedar vacío")]
         public String PaymentFolio { get; set; }
 
+        [Required(ErrorMessage = "El número de referencia no puede quedar vacío")]
         public String ReferenceId { get; set; }
+
+        [Required(ErrorMessage = "El nombre del banco no puede quedar vacío")]
         public String Bank { get; set; }
+
+        [Required(ErrorMessage = "El número de cuenta no puede quedar vacío")]
         public String AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "La fecha de depósito no puede quedar vacía")]
         public DateTime DocumentDate { get; set; }
+
+        [Required(ErrorMessage = "El monto depositado no puede quedar vacío")]
         public Decimal DepositAmount { get; set; }
     }
 }
--- PoliciesBlazorApp/PoliciesBlazorApp/Models/PolicyCommentPost.cs
+++ PoliciesBlazorApp/PoliciesBlazorApp/Models/PolicyCommentPost.cs
@@ -1,9 +1,13 @@
-﻿namespace PoliciesBlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PoliciesBlazorApp.Models
 {
     public class PolicyCommentPost
     {
         public String Policy { get; set; }
         public String Invoice { get; set; }
+
+        [Required(ErrorMessage = "El comentario no puede quedar vacío")]
         public String Comment { get; set; }
     }
 }
